7b145e15113b5d214d381bfb69d4c4149105370f
1stmuse <olysegs@gmail.com>
2020-09-08 23:14:11 -0400

    added the icon

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {View, Text, StyleSheet,Animated, Dimensions} from 'react-native'
 
-
 const {width, height} =Dimensions.get('window')
 
 const Main = ({info, index,x}) => {
